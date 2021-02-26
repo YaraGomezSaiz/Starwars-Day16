01ac78c9058fbfe81b42ab9e684298d9bf34f86b
YARA-G <y.gomez.saiz@hotmail.com>
2021-02-26 13:56:09 +0000

    project

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export default function Home() {
+	const [planets, setPlanets] = useState([]);
+	let BaseURL = "https://www.swapi.tech/api/";
+
+	// OBTENER LISTADO COMPLETO AL CARGAR LA PAGINA
+	useEffect(() => {
+		//method: GET para obtener el listado completo
+		fetch(BaseURL + "planets/", {
+			method: "GET", // *GET, POST, PUT, DELETE, etc.
+			headers: {
+				"Content-Type": "application/json"
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(responseJson => {
+				setPlanets(responseJson);
+			});
+	}, []);
+
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button, bootstrap is working
+			</a>
+		</div>
+	);
+}
